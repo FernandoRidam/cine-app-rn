7b291fae0afeed791c2ef76d88e41cc3ca8baa00
FernandoRidam <fernandohipalae71@gmail.com>
2021-02-04 15:07:50 -0300

    Select language

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -1,20 +1,60 @@
-import React from 'react';
+import React, {
+  useState,
+  useEffect,
+} from 'react';
 
 import {
   KeyboardAvoidingView,
   Platform,
+  View,
+  Text,
 } from 'react-native';
+
+import {
+  useTranslation,
+} from 'react-i18next';
+
+import {
+  Option,
+  Separator,
+} from '../../components';
 
 import Styles from './styles';
 
 export function Settings() {
+  const [ language, setLanguage ] = useState('pt');
+
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    i18n.changeLanguage( language );
+  }, [ language ]);
+
   return (
     <KeyboardAvoidingView
       style={ Styles.container }
       behavior="padding"
       enabled={ Platform.OS === 'ios' }
     >
-      {/*  */}
+      <View style={ Styles.menu }>
+        <Text style={ Styles.label }>{ t('screens.settings.language') }</Text>
+
+        <Separator />
+
+        <View style={ Styles.options }>
+          <Option
+            onPress={() => setLanguage('pt')}
+            label={ t('screens.settings.languages.pt')}
+            selected={ language === 'pt'}
+          />
+
+          <Option
+            onPress={() => setLanguage('en')}
+            label={ t('screens.settings.languages.en')}
+            selected={ language === 'en'}
+          />
+        </View>
+      </View>
     </KeyboardAvoidingView>
   );
 };
