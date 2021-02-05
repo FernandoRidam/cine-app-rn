77748de6440167c6f476660626b876158ace5c6b
FernandoRidam <fernandohipalae71@gmail.com>
2021-02-05 12:36:12 -0300

    Remove export FloatButtom

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,4 @@ export * from './Separator';
 export * from './TrendingCard';
 export * from './GenreSection';
 export * from './Loading';
-export * from './FloatButton';
 export * from './CardFilm';
